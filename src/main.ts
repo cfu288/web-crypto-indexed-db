c05066f2818138f324158f2ed3411e89baec8f4c
Christopher Fu <invent288cfu@gmail.com>
2022-12-21 17:58:31 -0500

    feat: update text in index

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { setupStep2Display, removeStep2Display } from "./demo/step2-display";
 import { setupStep1Display } from "./demo/step1-display";
+import { setupStep2Display, removeStep2Display } from "./demo/step2-display";
 import { setupStep3Display, removeStep3Display } from "./demo/step3-display";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
